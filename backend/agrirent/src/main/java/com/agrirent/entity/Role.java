32e5fb32c535ef3d97b171eeb3f11755629be2e3
Rohith Biradar <rohithbiradar18@gmail.com>
2026-06-09 01:16:06 +0530

    Add User entity, repository and modified database design

---
+++ backend/agrirent/src/main/java/com/agrirent/entity/Role.java
@@ -0,0 +1,6 @@
+package com.agrirent.entity;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ backend/agrirent/src/main/java/com/agrirent/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.agrirent.repository;
+
+import com.agrirent.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsername(String username);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByUsername(String username);
+}
