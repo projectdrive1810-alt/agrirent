592189ec9a79a7f93316efe29be8bc2bb2cf26ea
Rohith Biradar <rohithbiradar18@gmail.com>
2026-06-08 18:04:33 +0530

    Setup Spring Boot project and PostgreSQL configuration

---
+++ backend/agrirent/src/main/java/com/agrirent/AgrirentApplication.java
@@ -0,0 +1,13 @@
+package com.agrirent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrirentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrirentApplication.class, args);
+	}
+
+}
---
+++ backend/agrirent/src/test/java/com/agrirent/AgrirentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agrirent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrirentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
