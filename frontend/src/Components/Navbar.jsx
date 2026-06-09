d5b53637b4322d63d0a8af2fb09f653eaabe173b
Keerthi <keerthikv018@gmail.com>
2026-06-09 15:02:33 +0530

    Add Navbar

---
+++ frontend/src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">AgriRent</div>
+      <div className="navbar-links">
+        {/* Navigation links will go here */}
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
